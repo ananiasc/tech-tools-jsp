940c513308b75701954da46e504acd5eae80eff5
Ananias Caetano <ananiascaetano556@gmail.com>
2024-09-07 23:57:41 -0300

    first commit

---
+++ src/main/java/br/com/ananiascaetano/TechToolsWeb/TechToolsWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.ananiascaetano.TechToolsWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechToolsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechToolsWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ananiascaetano/TechToolsWeb/TechToolsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ananiascaetano.TechToolsWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechToolsWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
