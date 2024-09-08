6c6cd80beb6764a8bd9fde366e3d14f2e77b0f27
Ananias Caetano <ananiascaetano556@gmail.com>
2024-09-08 10:48:47 -0300

    feat: mvc jsp and rename package

--- src/main/java/br/com/ananiascaetano/TechToolsWebApplication.java
+++ src/main/java/br/com/ananiascaetano/TechToolsWebApplication.java
@@ -1,4 +1,4 @@
-package br.com.ananiascaetano.TechToolsWeb;
+package br.com.ananiascaetano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/ananiascaetano/TechToolsWebApplicationTests.java
+++ src/test/java/br/com/ananiascaetano/TechToolsWebApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.ananiascaetano.TechToolsWeb;
+package br.com.ananiascaetano;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
